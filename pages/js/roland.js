e4282a05f94d03e34d29a4418e18a189d475f6da
AnnamTRAN <annampierretran@gmail.com>
2023-09-15 15:05:48 +0200

    :sparkles: - Parallax intro

--- pages/js/roland.js
+++ pages/js/roland.js
@@ -1,9 +1,22 @@
+// navbar mobile ver
 const header = document.getElementById('header')
+const body = document.querySelector('body')
 
 const display = () =>{
     if (header.style.transform == "translateX(0%)"){
         header.style.transform = "translateX(-100%)"
+        body.style.overflow = "visible"
     } else{
+        body.style.overflow = "hidden"
         header.style.transform = "translateX(0%)"
     }
 }
+
+// parallax effect
+const title = document.getElementById('title')
+
+window.addEventListener('scroll', () =>{
+    let scroll = window.scrollY
+
+    title.style.top = scroll * 0.40 + "px"
+})
