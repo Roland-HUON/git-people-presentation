d2ad2962334bdb59e9afdf9b523ccfc321102136
Roland HUON <huonroland91@gmail.com>
2023-09-16 20:58:47 +0200

    Responsive fait

---
+++ pages/js/script.js
@@ -0,0 +1,6 @@
+const burgerIcon = document.querySelector('.burgerIcon')
+const booksmark = document.querySelectorAll('.booksmark')
+
+burgerIcon.addEventListener('click', ()=>{
+    booksmark.style.display = "block"
+})
